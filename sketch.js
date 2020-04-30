c26e9fcc815c240e58c5112907765dcbb1d1a374
Vivek Sharma <thekeviv.vivek@gmail.com>
2020-04-29 22:25:04 -0700

    Added sun and made earth, moon revolve around it

--- sketch.js
+++ sketch.js
@@ -10,13 +10,13 @@ const settings = {
   // Make the loop animated
   animate: true,
   // Get a WebGL canvas rather than 2D
-  context: "webgl"
+  context: "webgl",
 };
 
 const sketch = ({ context }) => {
   // Create a renderer
   const renderer = new THREE.WebGLRenderer({
-    canvas: context.canvas
+    canvas: context.canvas,
   });
 
   // WebGL background color
@@ -24,7 +24,7 @@ const sketch = ({ context }) => {
 
   // Setup a camera
   const camera = new THREE.PerspectiveCamera(50, 0.5, 0.01, 100);
-  camera.position.set(0, 4, -4);
+  camera.position.set(0, 4, -15);
   camera.lookAt(new THREE.Vector3());
 
   // Setup camera controller
@@ -36,31 +36,43 @@ const sketch = ({ context }) => {
   // Setup a geometry
   const geometry = new THREE.SphereGeometry(1, 32, 16);
 
-  const textureLoader = new THREE.TextureLoader()
-  const earthTexture = textureLoader.load('earth.jpg')
-  const moonTexture = textureLoader.load('moon.jpg')
+  const textureLoader = new THREE.TextureLoader();
+  const earthTexture = textureLoader.load("earth.jpg");
+  const moonTexture = textureLoader.load("moon.jpg");
+  const sunTexture = textureLoader.load("sun.jpg");
 
   // Setup earth material
   const earthMaterial = new THREE.MeshBasicMaterial({
-    map: earthTexture
+    map: earthTexture,
   });
 
   //setup moon material
   const moonMaterial = new THREE.MeshBasicMaterial({
-    map: moonTexture
-  })
+    map: moonTexture,
+  });
 
-  // Setup a mesh with geometry + material
-  const earthMesh = new THREE.Mesh(geometry, earthMaterial);
-  scene.add(earthMesh);
-
-
-  const moonGroup = new THREE.Group()
+  const sunMaterial = new THREE.MeshBasicMaterial({
+    map: sunTexture,
+  });
+  const sunMesh = new THREE.Mesh(geometry, sunMaterial);
+  sunMesh.scale.setScalar(4);
   const moonMesh = new THREE.Mesh(geometry, moonMaterial);
-  moonMesh.position.set(3, 0, 0)
-  moonMesh.scale.setScalar(0.25)
-  moonGroup.add(moonMesh)
-  scene.add(moonGroup)
+  moonMesh.scale.setScalar(0.5);
+  const earthMesh = new THREE.Mesh(geometry, earthMaterial);
+
+  const solarSystemGroup = new THREE.Group();
+  solarSystemGroup.add(sunMesh);
+  const planetsGroup = new THREE.Group();
+  const planetBodiesGroup = new THREE.Group();
+  planetBodiesGroup.position.set(0, 0, -8);
+  planetBodiesGroup.add(earthMesh);
+  planetBodiesGroup.add(moonMesh);
+  moonMesh.position.set(0, 0, -2);
+  planetsGroup.add(planetBodiesGroup);
+  solarSystemGroup.add(planetsGroup);
+  scene.add(solarSystemGroup);
+  scene.add(new THREE.GridHelper(5, 50));
+  scene.add(new THREE.AxesHelper(5));
 
   // draw each frame
   return {
@@ -73,9 +85,10 @@ const sketch = ({ context }) => {
     },
     // Update & render your scene here
     render({ time }) {
-      moonGroup.rotation.y = time * 0.1
-      moonMesh.rotation.y = time * 0.25
-      earthMesh.rotation.y = time * 0.1
+      planetsGroup.rotation.y = time * 0.125;
+      planetBodiesGroup.rotation.y = time * 0.5;
+      moonMesh.rotation.y = time * 0.1;
+      earthMesh.rotation.y = time * 0.05;
       controls.update();
       renderer.render(scene, camera);
     },
@@ -83,7 +96,7 @@ const sketch = ({ context }) => {
     unload() {
       controls.dispose();
       renderer.dispose();
-    }
+    },
   };
 };
 
